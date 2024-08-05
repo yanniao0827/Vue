6394e047cccb7317ba53f8065b75657538785413
Leana <yanning0113niao@gmail.com>
2024-08-05 14:01:32 +0800

    7-1.雙向綁定

--- PRACTICE/my_app/src/App.vue
+++ PRACTICE/my_app/src/App.vue
@@ -5,16 +5,16 @@ import LifeNote from './components/LifeNote.vue';
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
+  </div> -->
   <!-- <HelloWorld msg="Vite + Vue" /> -->
-  <myButton />
+  <!-- <myButton /> -->
   <LifeNote />
 </template>
 
--- PRACTICE/my_app/src/components/LifeNote.vue
+++ PRACTICE/my_app/src/components/LifeNote.vue
@@ -1,12 +1,17 @@
 <script setup>
-import { ref } from 'vue';
+import { ref,onUpdated,onUnmounted } from 'vue';
 const note=ref('')
+const updateLocalStorage = () => {
+    // 我希望可以存取我即時輸入的筆記
+    localStorage.setItem('userNote', note.value);
+}
 </script>
 
 // 用{{}}包起來是vue的插值語法
+// v-model為雙向綁定的語法，在這裡雙向綁訂了input以及{{note}}，此時在input輸入文字會值時顯示在p標籤裡面
 <template>
 <h1>筆記</h1>
-<input type="text" placeholder="輸入筆記">
+<input type="text" placeholder="輸入筆記" v-model="note">
 <p>Note:{{ note }}</p>
 </template>
 
