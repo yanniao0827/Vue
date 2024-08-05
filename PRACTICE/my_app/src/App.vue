b36378c2a4533f657cdff03e6d88301c6eee1a08
Leana <yanning0113niao@gmail.com>
2024-08-05 11:31:06 +0800

    5.新增按鈕組件

--- PRACTICE/my_app/src/App.vue
+++ PRACTICE/my_app/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import myButton from './components/myButton.vue';
 </script>
 
 <template>
@@ -11,7 +12,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <myButton text="Click me" />
 </template>
 
 <style scoped>
---
+++ PRACTICE/my_app/src/components/myButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {ref} from 'vue'
+const text=ref('點我')
+</script>
+
+<template>
+<button id="show">{{text}}</button>
+</template>
+
+<style scoped>
+#show{
+    background-color: #1A394A;
+    color: #FFF7E9;
+}
+</style>
