60fee737e23e3cdfc33616a2ad2a72791103198f
Leana <yanning0113niao@gmail.com>
2024-08-05 15:15:04 +0800

    7.8v-if、v-else

--- PRACTICE/my_app/src/components/LifeNote.vue
+++ PRACTICE/my_app/src/components/LifeNote.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref,onUpdated,onUnmounted } from 'vue';
 const note=ref('')
+const text=ref('輸入樣式')
 const a=ref(30)
 const b=ref(40)
 // 寫已定義好的樣式名稱
 let classStyle=ref('font50')
+let isFont30=ref(false)
 
 const testStyle=ref({color:'tomato', fontSize:'25px', fontWeight:'bold'})
 const updateLocalStorage = () => {
@@ -12,8 +14,13 @@ const updateLocalStorage = () => {
     localStorage.setItem('userNote', note.value);
 }
 onUpdated(()=>{
+    if(note=='font30'){
+        isFont30=true
+    }else{
+        isFont30=false
+    }
     updateLocalStorage();
-    console.log('筆記已更新');
+    console.log('isFont30',isFont30);
 })
 // 確保關閉前也有更新LocalStorage
 onUnmounted(()=>{
@@ -25,12 +32,15 @@ onUnmounted(()=>{
 // 用{{}}包起來是vue的插值語法
 // v-model為雙向綁定的語法，在這裡雙向綁訂了input以及{{note}}，此時在input輸入文字會值時顯示在p標籤裡面
 // v-bind為樣式繫節語法，:style="(自訂的樣式名稱)"就可以代入自訂樣式，v-bind可以省略
+// v-if、v-else為條件渲染語法，當條件為true時才會顯示，這裡設定成文字等於font30時才顯示，因此font10被隱藏
 <template>
 <h1>筆記</h1>
-<input type="text" placeholder="輸入筆記" v-model="note">
+<input type="text" :placeholder="text" v-model="note">
 <p :style="testStyle">Note:{{ note }}</p>
 <p :class="classStyle">Calculate one: {{ 2+4 }}</p>
 <p>Calculate two: {{ a*b }}</p>
+<p v-if="isFont30 ==true">font10</p>
+<p v-else>font30</p>
 </template>
 
 
