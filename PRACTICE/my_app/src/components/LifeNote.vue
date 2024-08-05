ba3e44238c65deb1e756072063b6dd731e066b9d
Leana <yanning0113niao@gmail.com>
2024-08-05 14:29:42 +0800

    7.5樣式繫節

--- PRACTICE/my_app/src/components/LifeNote.vue
+++ PRACTICE/my_app/src/components/LifeNote.vue
@@ -3,6 +3,8 @@ import { ref,onUpdated,onUnmounted } from 'vue';
 const note=ref('')
 const a=ref(30)
 const b=ref(40)
+
+const testStyle=ref({color:'tomato', fontSize:'25px', fontWeight:'bold'})
 const updateLocalStorage = () => {
     // 我希望可以存取我即時輸入的筆記到localStorage
     localStorage.setItem('userNote', note.value);
@@ -20,10 +22,11 @@ onUnmounted(()=>{
 
 // 用{{}}包起來是vue的插值語法
 // v-model為雙向綁定的語法，在這裡雙向綁訂了input以及{{note}}，此時在input輸入文字會值時顯示在p標籤裡面
+// v-bind為樣式繫節語法，:style="(自訂的樣式名稱)"就可以代入自訂樣式 
 <template>
 <h1>筆記</h1>
 <input type="text" placeholder="輸入筆記" v-model="note">
-<p>Note:{{ note }}</p>
+<p v-bind:style="testStyle">Note:{{ note }}</p>
 <p>Calculate one: {{ 2+4 }}</p>
 <p>Calculate two: {{ a*b }}</p>
 </template>
