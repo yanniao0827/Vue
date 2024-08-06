2aa9c684b574b4ef13010bf6dfd6ff835b7c164b
Leana <yanning0113niao@gmail.com>
2024-08-06 15:00:17 +0800

    0806 4-6.分別由父、子組件呼叫pinia資料

--- PRACTICE/form_validate/src/App_pinia.vue
+++ PRACTICE/form_validate/src/App_pinia.vue
@@ -1,9 +1,16 @@
 <script setup>
 import PiniaStore from './components/PiniaStore.vue';
+import { testStore } from './stores/testStore';
+import { ref } from 'vue';
+const todoStore = testStore();
 </script>
 
 <template>
 <PiniaStore />
+<h3>從父組件找pinia資料</h3>
+<ul class="list">
+  <li v-for="item in todoStore.todos">{{ item.text }}</li>
+</ul>
 </template>
 
 <style scoped>
--- PRACTICE/form_validate/src/components/PiniaStore.vue
+++ PRACTICE/form_validate/src/components/PiniaStore.vue
@@ -2,11 +2,11 @@
 import {ref} from 'vue'
 // 這裡的testStore是在stores/testStore中就取好名，不能更改
 import {testStore} from '../stores/testStore';
+// 讓testStore的值可以在這個檔案中被引用
 const todoStore=testStore();
 console.log(todoStore);
 
 </script>
-
 
 <template>
     <h1>Pinia狀態管理</h1>
@@ -14,7 +14,7 @@ console.log(todoStore);
         <input type="text">
         <button>新增待辦</button>
         <ul class="list">
-            <li class="item">01</li>
+            <li class="item" v-for="item in todoStore.todos">{{ item.text }}</li>
             <li class="item">02</li>
             <li class="item">03</li>
         </ul>
