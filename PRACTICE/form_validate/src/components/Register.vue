1223c399ab3e640a085f5d9cc14b9afcfd37db89
Leana <yanning0113niao@gmail.com>
2024-08-06 10:21:22 +0800

    0806 1-3.使用@focus

--- PRACTICE/form_validate/src/components/Register.vue
+++ PRACTICE/form_validate/src/components/Register.vue
@@ -4,6 +4,23 @@ const name=ref('');
 const bday=ref('');
 const phone=ref('');
 const error=reactive([true,true,true]);
+function err(){
+    if(name.value== ''){
+        error[0]=false;
+    }else{
+        error[0]=true;
+    }
+    if(bday.value== ''){
+        error[1]=false;
+    }else{
+        error[1]=true;
+    }
+    if(phone.value== ''){
+        error[2]=false;
+    }else{
+        error[2]=true;
+    }
+}
 </script>
 
 <template>
@@ -11,18 +28,18 @@ const error=reactive([true,true,true]);
         <form>
   <div class="mb-3">
     <label for="name" class="form-label">姓名</label>
-    <input v-model="name" type="text" class="form-control" id="name" aria-describedby="emailHelp">
-    <div v-show="name.length== 0" id="emailHelp" class="form-text text-danger" :class="{'d-none':error[0]}">請輸入姓名</div>
+    <input v-model="name" type="text" @focus="err" class="form-control" id="name" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text text-danger" :class="{'d-none':error[0]}">請輸入姓名</div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">生日</label>
-    <input v-model="bday" type="text" class="form-control" id="bday">
-    <div v-show="bday.length== 0" id="emailHelp" class="form-text text-danger" :class="{'d-none':error[1]}">請選擇生日</div>
+    <input v-model="bday" type="text" @focus="err" class="form-control" id="bday">
+    <div id="emailHelp" class="form-text text-danger" :class="{'d-none':error[1]}">請選擇生日</div>
   </div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">電話</label>
-    <input v-model="phone" type="text" class="form-control" id="phone" aria-describedby="emailHelp">
-    <div v-show="phone.length== 0" id="emailHelp" class="form-text text-danger" :class="{'d-none':error[2]}">請輸入電話</div>
+    <input v-model="phone" type="text" @focus="err" class="form-control" id="phone" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text text-danger" :class="{'d-none':error[2]}">請輸入電話</div>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
