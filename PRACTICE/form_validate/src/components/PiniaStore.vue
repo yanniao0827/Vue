e6d3d204325390b99218cd249826b6e8a8d1e1ca
Leana <yanning0113niao@gmail.com>
2024-08-06 14:24:46 +0800

    0806 4-4.設定PiniaStore.vue

--- PRACTICE/form_validate/src/components/PiniaStore.vue
+++ PRACTICE/form_validate/src/components/PiniaStore.vue
@@ -3,6 +3,23 @@
 
 <template>
     <h1>Pinia狀態管理</h1>
+    <div class="container">
+        <input type="text">
+        <button>新增待辦</button>
+        <ul class="list">
+            <li class="item">01</li>
+            <li class="item">02</li>
+            <li class="item">03</li>
+        </ul>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+input{
+    width: 100%;
+}
+button{
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+</style>
