86b8600d92a1e473c67208fb60c5501f692f86f0
Leana <yanning0113niao@gmail.com>
2024-08-07 16:37:17 +0800

    0807 2-12.完成edit的router(小瑕疵)

--- PRACTICE/note/src/components/NoteList.vue
+++ PRACTICE/note/src/components/NoteList.vue
@@ -16,12 +16,14 @@ const {markPin}=todoStore;
     <div class="important mb-4">
         <h4><i class="fa-solid fa-bookmark me-2 text-danger"></i>重要筆記</h4>
         <ul class="list-group">
-            <li v-for="pin in finishNotes" class="list-group-item d-flex justify-content-between">
+            <li v-for="pin in finishNotes" class="list-group-item ">
+                <router-link :to="{name:'edit',params:{id:pin.id}}" class="text-dark d-flex justify-content-between">
                 {{pin.title}}
-            <div class="icon-group ">
-                <i class="fa-solid fa-heart me-3 text-danger" @click="markPin(pin.id)"></i>
-                <i class="fa-solid fa-trash"></i>
-            </div>
+                <div class="icon-group ">
+                    <i class="fa-solid fa-heart me-3 text-danger" @click="markPin(pin.id)"></i>
+                    <i class="fa-solid fa-trash"></i>
+                </div>
+                </router-link>
             </li>
         </ul>
     </div>
@@ -29,11 +31,14 @@ const {markPin}=todoStore;
     <div class="all">
         <h4><i class="fa-solid fa-book me-2"></i>所有筆記</h4>
         <ul class="list-group">
-            <li v-for="note in todoStore.notes" class="list-group-item d-flex justify-content-between">{{ note.title }} 
-            <div class="icon-group ">
-                <i class="fa-solid fa-heart me-3" @click="markPin(note.id)"></i>
-                <i class="fa-solid fa-trash"></i>
-            </div>
+            <li v-for="note in todoStore.notes" class="list-group-item">
+            <router-link  :to="{ name: 'edit', params: { id: note.id } }" class="text-dark d-flex justify-content-between">
+                {{note.title}}
+                <div class="icon-group ">
+                    <i class="fa-solid fa-heart me-3" @click="markPin(note.id)"></i>
+                    <i class="fa-solid fa-trash"></i>
+                </div>
+            </router-link>
             </li>
         </ul>
     </div>
